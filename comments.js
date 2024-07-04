f21ef8377c8d9b48503fd44e39cfb7912aa1391d
Benjamin Kearns <43222045+BenjaminJamesKearns@users.noreply.github.com>
2024-07-04 13:32:48 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// For parsing POST request
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Import comments.js file
+const comments = require('./comments.js');
+
+// Get all comments
+app.get('/comments', (req, res) => {
+    res.json(comments)
